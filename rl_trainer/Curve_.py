531abeb0e15cc6e820cb36878c9e9a68d74e7493
Your Name <is0541hx@ed.ritsumei.ac.jp>
2021-08-21 02:10:13 +0900

    first submit

---
+++ rl_trainer/Curve_.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import time,random
+import os 
+
+def cross_loss_curve(total_reward,a_loss,c_loss):
+    
+    plt.plot(np.array(c_loss), c='b', label='critic_loss', linewidth=0.2)
+    plt.plot(np.array(a_loss), c='g', label='actor_loss', linewidth=0.2)
+    plt.plot(np.array(total_reward), c='r', label='total_rewards', linewidth=0.2)
+    plt.legend(loc='best')
+    #plt.ylim(-15,15)
+    #plt.ylim(-0.25,0.05)
+    plt.ylabel('loss/10 & reward') 
+    plt.xlabel('Training Episode')
+    plt.grid()
+    plt.savefig("history.png")
+    plt.close()
---
+++ rl_trainer/algo/network.py
@@ -0,0 +1,119 @@
+from pathlib import Path
+import sys
+base_dir = Path(__file__).resolve().parent.parent
+sys.path.append(str(base_dir))
+from common import *
+import torch.nn as nn
+import torch.nn.functional as F
+
+HIDDEN_SIZE = 256
+
+
+class Actor(nn.Module):
+    def __init__(self, obs_dim, act_dim, num_agents, args, output_activation='tanh'):
+        super().__init__()
+        self.obs_dim = obs_dim
+        self.act_dim = act_dim
+        self.num_agents = num_agents
+
+        self.args = args
+
+        sizes_prev = [obs_dim, HIDDEN_SIZE]
+        middle_prev = [HIDDEN_SIZE, HIDDEN_SIZE]
+        sizes_post = [HIDDEN_SIZE << 1, HIDDEN_SIZE, act_dim]
+
+        self.prev_dense = mlp(sizes_prev)
+
+        if self.args.algo == "bicnet":
+            self.comm_net = LSTMNet(HIDDEN_SIZE, HIDDEN_SIZE)
+            sizes_post = [HIDDEN_SIZE << 1, HIDDEN_SIZE, act_dim]
+
+        elif self.args.algo == "ddpg":
+            sizes_post = [HIDDEN_SIZE, HIDDEN_SIZE, act_dim]
+
+        self.prev_dense = mlp(sizes_prev)
+        self.post_dense = mlp(sizes_post, output_activation=output_activation)
+
+    def forward(self, obs_batch):
+        out = self.prev_dense(obs_batch)
+
+        if self.args.algo == "bicnet":
+            out = self.comm_net(out)
+
+        out = self.post_dense(out)
+        return out
+
+
+class Critic(nn.Module):
+    def __init__(self, obs_dim, act_dim, num_agents, args):
+        super().__init__()
+
+        self.obs_dim = obs_dim
+        self.act_dim = act_dim
+        self.num_agents = num_agents
+
+        self.args = args
+
+        sizes_prev = [obs_dim + act_dim, HIDDEN_SIZE]
+
+        if self.args.algo == "bicnet":
+            self.comm_net = LSTMNet(HIDDEN_SIZE, HIDDEN_SIZE)
+            sizes_post = [HIDDEN_SIZE << 1, HIDDEN_SIZE, 1]
+
+        elif self.args.algo == "ddpg":
+            sizes_post = [HIDDEN_SIZE, HIDDEN_SIZE, 1]
+
+        self.prev_dense = mlp(sizes_prev)
+        self.post_dense_1 = mlp(sizes_post)
+        self.post_dense_2 = mlp(sizes_post)
+
+    def forward(self, obs_batch, action_batch):
+        out = torch.cat((obs_batch, action_batch), dim=-1)
+        out = self.prev_dense(out)
+
+        if self.args.algo == "bicnet":
+            out = self.comm_net(out)
+
+        out_1 = self.post_dense_1(out)
+        out_2 = self.post_dense_2(out)
+        return out_1,out_2 
+
+
+class DUTNet(nn.Module):
+    def __init__(self,
+                 input_size,
+                 hidden_size,
+                 batch_first=True,
+                 bidirectional=True):
+        super().__init__()
+
+        self.l1 = nn.Linear(input_size, 256)
+        self.l2 = nn.Linear(256, 256)
+        self.l3 = nn.Linear(256, hidden_size)
+
+    def forward(self, data, ):
+        x = F.relu(self.l1(data)) 
+        x = F.relu(self.l2(x)) 
+        output = F.relu(self.l3(x)) 
+        return output
+
+
+
+class LSTMNet(nn.Module):
+    def __init__(self,
+                 input_size,
+                 hidden_size,
+                 batch_first=True,
+                 bidirectional=True):
+        super(LSTMNet, self).__init__()
+
+        self.lstm = nn.LSTM(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            batch_first=batch_first,
+            bidirectional=bidirectional
+        )
+
+    def forward(self, data, ):
+        output, (_, _) = self.lstm(data)
+        return output
